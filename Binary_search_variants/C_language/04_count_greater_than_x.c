4a2ae6f7a54cab1ece47da84331c2cf93e5a5f81
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-21 17:31:23 +0200

    Create 04_count_greater_than_x.c

---
+++ Binary_search_variants/C_language/04_count_greater_than_x.c
@@ -0,0 +1,19 @@
+/* Number of elements > X */
+
+#include <stdio.h>
+
+int countGreaterThanX(int arr[], int n, int x) {
+    int l = 0, r = n;
+    while (l < r) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] <= x) l = mid + 1;
+        else r = mid;
+    }
+    return n - l;
+}
+
+int main() {
+    int arr[] = {1, 2, 4, 5, 7};
+    int n = 5, x = 4;
+    printf("Count > %d = %d\n", x, countGreaterThanX(arr, n, x));
+}
