adc5a7ab8939ada0dfd924cb065f3e6c0f2a6756
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-21 17:52:23 +0200

    Create 02_upper_bound.cpp

---
+++ Binary_search_variants/02_upper_bound.cpp
@@ -0,0 +1,21 @@
+/* Upper Bound Implementation */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int upperBound(vector<int>& arr, int x) {
+    int l = 0, r = arr.size();
+    while (l < r) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] <= x) l = mid + 1;
+        else r = mid;
+    }
+    return l;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 4, 4, 5, 7};
+    int x = 4;
+    cout << "Upper Bound Index: " << upperBound(arr, x) << endl;
+}
