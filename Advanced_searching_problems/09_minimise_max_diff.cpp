7cf1b1e8a45d33fe62481c7fe1005eb71def40d3
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-21 18:04:20 +0200

    Create 09_minimise_max_diff.cpp

---
+++ Advanced_searching_problems/09_minimise_max_diff.cpp
@@ -0,0 +1,18 @@
+/* Minimise Maximum Difference among Subsets */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int minimizeMaxDiff(vector<int>& arr,int k){
+    sort(arr.begin(),arr.end());
+    int ans=INT_MAX;
+    for(int i=0;i+k-1<arr.size();i++){
+        ans=min(ans,arr[i+k-1]-arr[i]);
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr={10,100,300,200,1000,20,30};
+    int k=3;
+    cout<<"Min
