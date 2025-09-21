45e7d099efd62a390487efcc46f1ce6ff48e51b1
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-21 14:21:05 +0200

    Create 01_bubble_sort.cpp

---
+++ Basic_Sorting_problems/01_bubble_sort.cpp
@@ -0,0 +1,19 @@
+/* 1. Bubble Sort an array. */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n-1; i++)
+        for (int j = 0; j < n-1-i; j++)
+            if (arr[j] > arr[j+1]) swap(arr[j], arr[j+1]);
+}
+
+int main() {
+    vector<int> arr = {5, 2, 9, 1, 5, 6};
+    bubbleSort(arr);
+    for(int x: arr) cout << x << " ";
+    cout << endl;
+}
