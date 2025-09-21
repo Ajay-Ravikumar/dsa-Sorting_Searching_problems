34916e4c966c3b15f492b885f0474eede0d606d6
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-21 17:34:08 +0200

    Create 07_sqrt_binary.c

---
+++ Binary_search_variants/C_language/07_sqrt_binary.c
@@ -0,0 +1,18 @@
+/* Square root using binary search */
+
+#include <stdio.h>
+
+int mySqrt(int x) {
+    int l = 0, r = x, ans = 0;
+    while (l <= r) {
+        long mid = l + (r - l) / 2;
+        if (mid*mid <= x) { ans = mid; l = mid + 1; }
+        else r = mid - 1;
+    }
+    return ans;
+}
+
+int main() {
+    int x = 17;
+    printf("Sqrt: %d\n", mySqrt(x));
+}
