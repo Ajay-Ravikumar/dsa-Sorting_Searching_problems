78d5bc78f27126759fd0bc775089d71c7738c379
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-21 16:46:41 +0200

    Create 02_quick_sort_lomuto.cpp

---
+++ Efficient_Divide_Conquer_Sorting_problems/02_quick_sort_lomuto.cpp
@@ -0,0 +1,27 @@
+/* Quick Sort (Lomuto Partition) */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(vector<int>& arr,int l,int r){
+    int pivot=arr[r], i=l-1;
+    for(int j=l;j<r;j++) if(arr[j]<=pivot) swap(arr[++i],arr[j]);
+    swap(arr[i+1],arr[r]);
+    return i+1;
+}
+
+void quickSort(vector<int>& arr,int l,int r){
+    if(l<r){
+        int pi=partition(arr,l,r);
+        quickSort(arr,l,pi-1);
+        quickSort(arr,pi+1,r);
+    }
+}
+
+int main(){
+    vector<int> arr={5,2,9,1,5,6};
+    quickSort(arr,0,arr.size()-1);
+    for(int x:arr) cout<<x<<" ";
+    cout<<endl;
+}
