282c159f19ddf73deb8463adb7ac2ff7d024b0ae
Ajay-Ravikumar <ajay.ravikumar@yahoo.com>
2025-09-21 14:28:10 +0200

    Create 10_sort_desc.c

---
+++ Basic_Sorting_problems/C_Language/10_sort_desc.c
@@ -0,0 +1,17 @@
+/* 10. Sort array in descending order using basic sorting. */
+
+#include <stdio.h>
+
+void sortDesc(int arr[],int n){
+    for(int i=0;i<n-1;i++)
+        for(int j=0;j<n-1-i;j++)
+            if(arr[j]<arr[j+1]){ int t=arr[j]; arr[j]=arr[j+1]; arr[j+1]=t; }
+}
+
+int main(){
+    int arr[]={5,2,9,1,5,6};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    sortDesc(arr,n);
+    for(int i=0;i<n;i++) printf("%d ",arr[i]);
+    printf("\n");
+}
